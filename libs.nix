9c7ee8c3b272e34b4aea07eaa2a28fe6e38ecd75
Alexander Bich <quyse0@gmail.com>
2020-12-27 11:22:29 +0300

    change libs.nix interface to have stdenv explicitly specified

--- libs.nix
+++ libs.nix
@@ -1,5 +1,9 @@
-{ overrides ? {} }:
-rec {
+{ stdenv }:
+let
+  overrides = {
+    inherit stdenv;
+  };
+in rec {
   overlay = self: super: {
     # separate to not affect the rest of nixpkgs
     coil = rec {
