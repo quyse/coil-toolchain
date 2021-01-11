22a68d85388be1997905d0fd3669056e4c57f3e0
Alexander Bich <quyse0@gmail.com>
2021-01-11 15:23:19 +0300

    release.nix with touch config

---
+++ release.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {}
+}:
+let
+  root = import ./.;
+  llvm11 = root.llvm11 {
+    inherit pkgs;
+  };
+  windows = root.windows {
+    inherit pkgs;
+  };
+in {
+  touch = {
+    llvm11cc = llvm11.stdenv.cc;
+    inherit (windows) initialDisk;
+    inherit (windows.msvs) vs16BuildToolsCppDisk vs15BuildToolsCppDisk;
+  };
+}
