6c58971093bf4164cb65b1adc710ac349c0d1b22
Alexander Bich <quyse0@gmail.com>
2021-01-23 01:11:15 +0300

    build some other libs

--- libs.nix
+++ libs.nix
@@ -7,13 +7,16 @@ in rec {
   overlay = self: super: {
     # separate to not affect the rest of nixpkgs
     coil = rec {
+      expat = super.expat.override overrides;
       zlib = self.callPackage ./libs/zlib-ng overrides;
-
+      bzip2 = super.bzip2.override overrides;
+      icu = super.icu.override overrides;
+      # TODO: libiconv for boost
       boost = (super.boost17x.override (overrides // {
         enableShared = false;
         enableStatic = true;
         toolset = null;
-        inherit zlib;
+        inherit expat zlib bzip2 icu;
       })).overrideAttrs (attrs: {
         patches = (attrs.patches or [])
           ++ stdenv.lib.optionals (stdenv.hostPlatform != stdenv.buildPlatform) [./libs/boost/cross.patch]
