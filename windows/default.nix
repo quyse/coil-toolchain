bd3c7919f68b26835195e6584252c54dab639237
Alexander Bich <code@quyse.net>
2021-02-27 19:56:58 +0300

    vm: use unmap for drives

--- windows/default.nix
+++ windows/default.nix
@@ -120,7 +120,7 @@ windows = rec {
             # https://blog.wikichoon.com/2014/07/enabling-hyper-v-enlightenments-with-kvm.html
             [ "-cpu" "qemu64,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time" ]
             # main hdd
-            [ "-drive" "file=${output_directory}/packer-qemu,if=virtio,cache=unsafe,discard=ignore,format=qcow2,index=0" ]
+            [ "-drive" "file=${output_directory}/packer-qemu,if=virtio,cache=unsafe,discard=unmap,detect-zeroes=unmap,format=qcow2,index=0" ]
           ] ++
           # cdroms
           pkgs.lib.optionals (disk == null && iso != null) [
@@ -130,7 +130,7 @@ windows = rec {
             [ "-drive" "file=${virtio_win_iso},media=cdrom,index=2" ]
           ] ++
           # extra hdd
-          pkgs.lib.optional (extraDisk != null) [ "-drive" "file=${extraDisk},if=virtio,cache=unsafe,discard=ignore,format=qcow2,index=3" ];
+          pkgs.lib.optional (extraDisk != null) [ "-drive" "file=${extraDisk},if=virtio,cache=unsafe,discard=unmap,detect-zeroes=unmap,format=qcow2,index=3" ];
         }
         // (if disk != null then {
           disk_image = true;
