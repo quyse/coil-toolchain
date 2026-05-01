3573e40ed8d58a728aa975f4adac21bdca32dc67
Alexander Bich <code@quyse.net>
2026-05-01 23:40:07 +0900

    pnpm: fix for latest nixpkgs

--- js.nix
+++ js.nix
@@ -7,5 +7,13 @@ rec {
     repo = "pnpm2nix-nzbr";
     rev = "35f88a41d29839b3989f31871263451c8e092cb1";
     hash = "sha256-7Qzmy1snKbxFBKoqUrfyxxmEB8rPxDdV7PQwRiAR01o=";
-  }}/derivation.nix" {};
+  }}/derivation.nix" {
+    nodejs = pkgs.nodejs.overrideAttrs (old: {
+      passthru = (old.passthru or {}) // {
+        pkgs = ((old.passthru or {}).pkgs or {}) // {
+          pnpm = pkgs.pnpm;
+        };
+      };
+    });
+  };
 }
